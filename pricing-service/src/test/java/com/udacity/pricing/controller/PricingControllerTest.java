b80609213bcda27c3e90c843608ef9bc188eff07
Anubhav Jain <anubhav.j.619@gmail.com>
2021-07-12 18:13:39 +0530

    add pricing test

--- pricing-service/src/test/java/com/udacity/pricing/controller/PricingControllerTest.java
+++ pricing-service/src/test/java/com/udacity/pricing/controller/PricingControllerTest.java
@@ -1,5 +1,27 @@
 package com.udacity.pricing.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PricingControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testGetPrice() throws Exception {
+        mvc.perform(get("/services/price").param("vehicleId", "1"))
+                .andExpect(status().isOk());
+    }
 }
